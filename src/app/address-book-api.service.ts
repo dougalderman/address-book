4ea166fd4dce0d31f03d445def6b0cb4b46b23a1
Doug Alderman <agnadcp-d1@yahoo.com>
2022-09-05 19:47:40 -0400

    Removed unused import. Changed from start to flex-start.

--- src/app/address-book-api.service.ts
+++ src/app/address-book-api.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { catchError, Observable, map, forkJoin, of } from 'rxjs';
+import { catchError, Observable, map, of } from 'rxjs';
 
 import { ContactModel } from './contact-model';
 
